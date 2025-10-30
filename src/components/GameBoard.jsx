96912633e7a44f7d8cd736d0304965d3c358caec
Christian Joseph Dalisay <7392803+launchwithchris@users.noreply.github.com>
2025-10-30 19:04:18 +0800

    feat(game): lift gameBoard state up to App

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,47 +1,9 @@
 import { useState } from "react";
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectSquare(rowIndex, cellIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const newGameBoard = [
-  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //     ].map((row, rIndex) => {
-  //       if (rIndex === rowIndex) {
-  //         return row.map((cell, cIndex) => {
-  //           if (cIndex === cellIndex) {
-  //             return activePlayerSymbol;
-  //           }
-  //           return cell;
-  //         });
-  //       }
-  //       return row;
-  //     });
-  //     return newGameBoard;
-  //   });
-
-  //   onSelectSquare && onSelectSquare(rowIndex, cellIndex);
-  // }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <ol key={rowIndex} className="game-row">
           {row.map((playerSymbol, cellIndex) => (
             <li key={cellIndex} className="game-cell">
