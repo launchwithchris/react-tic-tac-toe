4cbec5bba49eacf59908d58e2b63d9b64aa77713
Christian Joseph Dalisay <7392803+launchwithchris@users.noreply.github.com>
2025-10-30 19:08:59 +0800

    feat(app): determine winner\ \ create winning combinations multi array with object properties row and col\ iterate winning combinations multi array and match with gameBoard if has three blocks adjacent and save the playerSymbol to winner\ print winner paragraph, in between Players and GameBoard compoent if a winner is determined

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { act, useState } from "react";
 
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
+
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function derivedActivePlayer(turns) {
   if (turns.length === 0) {
@@ -13,8 +21,34 @@ function derivedActivePlayer(turns) {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-
   const activePlayer = derivedActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].col];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
+    if (
+      firstSquareSymbol &&
+      secondSquareSymbol &&
+      thirdSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevGameTurns) => [
@@ -29,7 +63,7 @@ function App() {
   return (
     <main>
       <div id="game-container">
-        <ol id="players" class="highlight-player">
+        <ol id="players" className="highlight-player">
           <Player
             initialName="Player 1"
             symbol="X"
@@ -43,9 +77,10 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
+        {winner && <p>We won, {winner}!</p>}
         <GameBoard
           onSelectSquare={handleSelectSquare}
-          turns={gameTurns}
+          board={gameBoard}
           activePlayerSymbol={activePlayer}
         />
       </div>
